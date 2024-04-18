0fd4a23196e1bf2f72a0b9c54223c71d4a5408a4
alexluczi <luczialex@gmail.com>
2024-04-18 11:41:42 +0200

    Make: Boilerplate

--- tt_utazasi_ugynokseg/Program.cs
+++ tt_utazasi_ugynokseg/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace TTUtazasiUgynokseg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your code here
+        }
+    }
+}
